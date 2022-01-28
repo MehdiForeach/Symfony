846df1acbbb28b9d4a9912f9f7875e9580d85e1a
MehdiForeach <mehdi.birembaut.foreach@gmail.com>
2022-01-28 14:15:44 +0100

    maj

---
+++ src/Controller/DemoController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DemoController extends AbstractController
+{
+    /**
+     * @Route("/demo", name="demo")
+     */
+    public function index(): Response
+    {
+        return $this->render('demo/index.html.twig', [
+            'controller_name' => 'DemoController',
+        ]);
+    }
+
+    public function accueil(): Response
+    {
+        return $this->render('demo/accueil.html.twig', [
+            'controller_name' => 'DemoController',
+        ]);
+    }
+
+    public function damier(): Response
+    {
+        $ligne = 8;
+        $colone = 8;
+        $n=1;
+        $html = "<table class='dam'>\n";
+        for( $i=0 ; $i<$ligne ; $i++ )
+        {
+            // la ligne est-elle pair ?
+            if ( $i % 2 == 0 )
+            {
+                $html .= "<tr>\n";
+                for( $j=0 ; $j < $colone ; $j++ )
+                {
+                    // la colonne est-elle pair ?
+                    if ( $j % 2 == 0 )
+                        $class="class='noir'";
+                    else
+                        $class="class='blanc'";
+                    $html .= "<td $class>".$n."\n";
+                    $n++;
+                    $html .= "</td>\n";
+                }
+                $html .= "</tr>\n";
+            }
+            // sinon
+            else
+            {
+                $html .= "<tr>\n";
+                for( $j=0 ; $j < $colone ; $j++ )
+                {
+                    if ( $j % 2 == 0 )
+                        $class="class='blanc'";
+                    else
+                        $class="class='noir'";
+                    $html .= "<td $class>".$n."\n";
+                    $n++;
+                    $html .= "</td>\n";
+                }
+                $html .= "</tr>\n";
+            }
+        }
+        $html .= "</table>\n";
+
+        return $this->render('demo/damier.html.twig', [
+            'damier' => $html,
+        ]);
+    }
+}
